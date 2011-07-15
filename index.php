45619ea8b21b5eba014379203c231caf51ad204d
Rodrigo Santiago Motta <rsantiago897@gmail.com>
2011-07-15 15:29:16 -0300

    Arquivo para teste do método selectAll

--- index.php
+++ index.php
@@ -1,11 +1,13 @@
 <?php
 
-			/*  pagina de exemplo , para fim de teste. */ 
+			/*  
+			  * pagina de exemplo , para fim de teste. 
+			*/ 
 			
 			require_once('TDG.php');
 
 			$teste = TDG::selectAll('usuario'); // insert table name HERE.
 			
 			while($testes = mysql_fetch_array($teste)):
-					echo "${testes['login']} = ${testes['senha']} <br>";
+					echo "${testes['login']} = ${testes['senha']} <br>"; // insert the column table name.
 			endwhile;
